947b095b7dc25d18dbe4168fdc70092c97e6b26c
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2024-01-17 17:08:18 +0530

    - Added code to understand about pid

--- lib/product_stock.ex
+++ lib/product_stock.ex
@@ -1,18 +1,21 @@
 defmodule ProductStock do
-  @moduledoc """
-  Documentation for `ProductStock`.
-  """
+  def handle_stock(stock) do
+    receive do
+      {:add, from, product} ->
+        stock_update = [product] ++ stock
+        log(from, :add)
+        send(from, stock_update)
+        handle_stock(stock_update)
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ProductStock.hello()
-      :world
-
-  """
-  def hello do
-    :world
+      {:status, from} ->
+        log(from, :status)
+        send(from, stock)
+        handle_stock(stock)
+    end
   end
+
+  def log(pid, :add), do: IO.inspect("#{:erlang.pid_to_list(pid)} added new item to the stack ")
+
+  def log(pid, :status),
+    do: IO.inspect("#{:erlang.pid_to_list(pid)} checked the stack ")
 end
